be00d4ded2be149f91e3d35291034e51d3545e25
Facundo García Conejero <fgarciaconejero@gmail.com>
2021-01-22 20:51:33 -0300

    Fixed match start test GREEN

--- Classes/Match.cs
+++ Classes/Match.cs
@@ -4,14 +4,19 @@ namespace Classes
 {
     public class Match
     {
-        public Team HomeTeam { get; }
-        public Team AwayTeam { get; }
+        public Team HomeTeam { get; private set; }
+        public Team AwayTeam { get; private set; }
 
         public Match() { }
         public Match(Team homeTeam, Team awayTeam)
         {
             HomeTeam = homeTeam;
             AwayTeam = awayTeam;
+        }
+        public void StartMatch(Team homeTeam, Team awayTeam)
+        {
+            this.HomeTeam = homeTeam;
+            this.AwayTeam = awayTeam;
         }
     }
 }
