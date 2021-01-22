d5da23c391c619fda0c89c28180023ee2e7bbbc7
Facundo García Conejero <fgarciaconejero@gmail.com>
2021-01-22 20:43:57 -0300

    Added empty parameters constructor

--- Classes/Match.cs
+++ Classes/Match.cs
@@ -7,6 +7,7 @@ namespace Classes
         public Team HomeTeam { get; }
         public Team AwayTeam { get; }
 
+        public Match() { }
         public Match(Team homeTeam, Team awayTeam)
         {
             HomeTeam = homeTeam;
