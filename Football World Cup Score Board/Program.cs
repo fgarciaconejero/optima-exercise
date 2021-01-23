fd2d3045e2420c8cafc415ca567992ade50494fd
Facundo García Conejero <fgarciaconejero@gmail.com>
2021-01-22 21:29:22 -0300

    Added an initial console setup so that the user can interact with the program

--- Football World Cup Score Board/Program.cs
+++ Football World Cup Score Board/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Classes;
+using System;
+using System.Collections.Generic;
 
 namespace Football_World_Cup_Score_Board
 {
@@ -6,7 +8,57 @@ namespace Football_World_Cup_Score_Board
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int input = 0;
+            List<Match> matches = new List<Match>();
+
+            Console.WriteLine("Welcome to the Football World Cup Score Board!");
+            while (input != 9)
+            {
+                Menu();
+                input = EnterIntegerToConsole(false);
+
+                Console.Clear();
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter the first team's name.");
+                        string teamName = Console.ReadLine();
+                        Console.WriteLine("Please enter the first team's score.");
+                        input = EnterIntegerToConsole(false);
+                        break;
+
+                }
+
+            }
+            Console.WriteLine("Thanks for using Football World Cup Score Board.");
+            Console.WriteLine("Exiting...");
+        }
+
+        private static int EnterIntegerToConsole(bool format)
+        {
+            int input = 0;
+            while (format == false)
+            {
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                    format = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nWrong input type! Please, enter an integer.");
+                }
+            }
+            return input;
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Please type one of the following values:");
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine(" 1 - Start new match.\n");
+            Console.WriteLine(" 9 - Exit program.\n");
+            Console.WriteLine("------------------------------------------------------------------------ \n");
         }
     }
 }
