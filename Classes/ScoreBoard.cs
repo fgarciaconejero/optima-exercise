e9bd59cef2fef1237b115e5592ece9998d8cc1f2
Facundo García Conejero <fgarciaconejero@gmail.com>
2021-01-22 22:05:34 -0300

    Added missing score board class

---
+++ Classes/ScoreBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class ScoreBoard
+    {
+        // ScoreBoard properties
+        public List<Match> Matches { get; private set; }
+
+        // ScoreBoard properties
+        public ScoreBoard() { }
+
+        // ScoreBoard functions
+        public void StartMatch(List<Match> matches)
+        {
+        }
+    }
+}
