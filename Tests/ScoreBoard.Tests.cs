1037bc767e0475de32f79115f08acda83f609ead
Facundo García Conejero <fgarciaconejero@gmail.com>
2021-01-22 22:05:56 -0300

    Added ScoreBoard constructor RED test

---
+++ Tests/ScoreBoard.Tests.cs
@@ -0,0 +1,25 @@
+﻿using Classes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestFixture]
+    class ScoreBoardTests
+    {
+        [Test]
+        public void ScoreBoard_Constructor()
+        {
+            // Arrange
+            List<Match> matches = new List<Match>();
+
+            // Act
+            ScoreBoard scoreBoard = new ScoreBoard();
+
+            // Assert
+            Assert.AreEqual(scoreBoard.Matches, matches);
+        }
+    }
+}
