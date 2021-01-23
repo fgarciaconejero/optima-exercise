5e1da71f1a8d8d633a246082ff0a3889d533037c
Facundo García Conejero <fgarciaconejero@gmail.com>
2021-01-22 22:05:21 -0300

    Adding comments

--- Classes/Match.cs
+++ Classes/Match.cs
@@ -4,10 +4,14 @@ namespace Classes
 {
     public class Match
     {
+        // Match properties
         public Team HomeTeam { get; private set; }
         public Team AwayTeam { get; private set; }
 
+        // Match constructor
         public Match() { }
+
+        // Match functions
         public void StartMatch(Team homeTeam, Team awayTeam)
         {
             this.HomeTeam = homeTeam;
